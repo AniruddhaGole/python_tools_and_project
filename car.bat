4dc25f2b6e38f66b2c70e500dd733a63725f1af2
AniruddhaGole <112776276+AniruddhaGole@users.noreply.github.com>
2024-05-07 22:13:35 +0530

    Add files via upload

---
+++ car.bat
@@ -0,0 +1,14 @@
+@echo off
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c1.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c2.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c3.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c4.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c5.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c6.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c7.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c8.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/c10.pdf -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/code1.zip -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/code3.zip -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/code4.zip -O
+curl https://github.com/AniruddhaGole/python_tools_and_project/blob/main/code5.zip -O
